2cbf832d142445a3b4c5d85a06a4d6849cd66fd1
Bálint Szekeres <balint.szekeres@feat.agency>
2021-05-13 01:03:16 +0200

    preload css

--- src/css-injector.ts
+++ src/css-injector.ts
@@ -2,7 +2,7 @@ export class CssInjector {
 	public inject(html: string, base: string, path: string): string {
 		return html.replace(
 			'</head>',
-			`<link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="${base}${path}">\n</head>`
+			`<link rel="preload" as="style" href="${base}${path}">\n<link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="${base}${path}">\n</head>`
 		);
 	}
 }
