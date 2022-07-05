7debfcc7f0e6a579ec92a712d96cb95b8f213be5
Bálint Szekeres <szekeres@b4lint.hu>
2022-07-05 13:50:55 +0200

    handle Google Fonts text kit

--- src/css-parser.ts
+++ src/css-parser.ts
@@ -2,20 +2,40 @@ import type { Font } from "./types";
 
 export class CssParser {
 	private fontSrcRegex = /(?:https?:)?\/\/.+?\.(?:woff2?|eot|ttf|otf|svg)(?:[?#=]+\w*)?/gi;
+	private googleFontsKitSrcRegex = /https:\/\/fonts.gstatic.com\/l\/font\?kit=[a-z0-9&=]+/gi;
+
 	private fontFileRegex = /[^/]+$/;
+	private googleFontsFileRegex = /\?kit=([a-z0-9]+)/i;
 
 	public parse(cssContent: string, base: string, assetsDir: string): {[key: string]: Font} {
 		const fonts: {[key: string]: Font} = {};
+
 		const fontSrcMatches = cssContent.matchAll(this.fontSrcRegex);
+		const googleFontsKitSrcMatches = cssContent.matchAll(this.googleFontsKitSrcRegex);
 
 		if (fontSrcMatches) {
 			for (const fontSrcMatch of fontSrcMatches) {
-				const fontFileName = fontSrcMatch.toString().match(this.fontFileRegex)?.toString();
+				const url = fontSrcMatch.toString();
+				const filename = url.match(this.fontFileRegex)?.toString();
 
-				if (fontFileName) {
-					fonts[fontFileName] = {
-						url: fontSrcMatch.toString(),
-						localPath: base + assetsDir + '/' + fontFileName,
+				if (filename) {
+					fonts[filename] = {
+						url,
+						localPath: base + assetsDir + '/' + filename,
+					};
+				}
+			}
+		}
+
+		if (googleFontsKitSrcMatches) {
+			for (const googleFontsKitSrcMatch of googleFontsKitSrcMatches) {
+				const url = googleFontsKitSrcMatch.toString();
+				const filename = url.match(this.googleFontsFileRegex)?.[1]?.toString();
+
+				if (filename) {
+					fonts[filename + '.woff2'] = {
+						url,
+						localPath: base + assetsDir + '/' + filename + '.woff2',
 					};
 				}
 			}
